8331c2fabffcfaa1d927d21036055d54b07fd0a4
MShin1 <karasawa429@gmail.com>
2025-12-01 22:11:25 +0900

    Initial Commit

---
+++ FolderOrganizer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FolderOrganizer
+//
+//  Created by 望月慎一 on 2025/12/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FolderOrganizer/FolderOrganizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FolderOrganizerApp.swift
+//  FolderOrganizer
+//
+//  Created by 望月慎一 on 2025/12/01.
+//
+
+import SwiftUI
+
+@main
+struct FolderOrganizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
