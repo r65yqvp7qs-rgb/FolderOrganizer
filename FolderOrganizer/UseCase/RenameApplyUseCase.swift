e8d36ff7fa083b8bfbc703dcf44c3031725d99a6
Tinasha <r65yqvp7qs@privaterelay.appleid.com>
2026-01-21 22:53:28 +0900

    不要な未使用ファイルを整理・削除（ビルド影響なし）
    ・コメントのみで実装のない UseCase / Utility ファイルを削除
    ・参照されていない View / Service を整理
    ・現行ビルド・ランに影響がないことを確認

--- FolderOrganizer/UseCase/RenameApplyUseCase.swift
+++
@@ -1,7 +0,0 @@
-//
-//  RenameApplyUseCase.swift
-//  FolderOrganizer
-//
-//  Created by 望月慎一 on 2025/12/30.
-//
-
--- FolderOrganizer/Utilities/KeyboardExtension.swift
+++
@@ -1,7 +0,0 @@
-//
-//  KeyboardExtension.swift
-//  FolderOrganizer
-//
-//  Created by 望月慎一 on 2026/01/14.
-//
-
